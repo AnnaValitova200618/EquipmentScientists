3cd74a4a8ea41a506c992bf05062ff588c039514
AnnaValitova200618 <93112045+AnnaValitova200618@users.noreply.github.com>
2023-05-13 16:38:03 +1000

    uppppp

---
+++ Equipment_Client/VM/DiagrammVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveCharts;
+namespace Equipment_Client.VM
+{
+    public class DiagrammVM
+    {
+        public SeriesCollection SeriesViews { get; set; }
+    }
+}
